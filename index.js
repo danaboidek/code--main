9dfe80dc47d39ae10674b68567038258d5387c5f
danaboidek <danaboidek1@gmail.com>
2021-05-19 16:14:22 -0500

    jquary animation

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+
+
+//  option1  
+
 var waypoint = new Waypoint({
-    element: document.getElementById('waypoint'),
+    element: document.getElementById('GirlScouts'),
     handler: function(direction) {
-      console.log('Scrolled to waypoint!')
-    }
-  })
+     $(".gridBg").css("left" , "0").css("opacity" , "1");
+    },
+    offset: 300 
+
+  });
+
+  
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('USDA'),
+    handler: function(direction) {
+     $(".gridBgg").css("left" , "0").css("opacity" , "1");
+    },
+    offset: 300 
+
+  });
+
+
+
+var waypoint = new Waypoint({
+    element: document.getElementById('Rambler'),
+    handler: function(direction) {
+     $(".gridBgrambler").css("left" , "0").css("opacity" , "1");
+    },
+    offset: 300 
+
+  });
